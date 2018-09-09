03d2c7cf776b8b503c7bf1a7d6216b5bf1b5549a
shibatadaiki <shibatadaiki92@gmail.com>
2018-09-09 22:00:33 +0900

    taskのCRUD実装（未挙動確認）

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,48 @@
+class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.order(:created_at)
+  end
+
+  def show; end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit; end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to @task, notice: t("layout.task.notice_create")
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to @task, notice: t("layout.task.notice_update")
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_url, notice: t("layout.task.notice_destroy")
+  end
+
+  private
+
+  def set_task
+    @task = task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+end
