9bad61fbac3e440b9887f14a5ff4703ddc9d0725
shibatadaiki <shibatadaiki92@gmail.com>
2018-09-26 15:45:07 +0900

    オプション要件・ラベルの付け外し

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'bcrypt'
 gem 'bootsnap', '>= 1.1.0', require: false
 gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
+gem 'jquery-rails'
 gem 'kaminari'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.11'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   include SessionsHelper
-
   def require_logged_in
     redirect_to new_session_path, notice: t("layout.session.require_login") unless logged_in?
   end
